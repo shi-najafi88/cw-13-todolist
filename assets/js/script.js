f854a38bdac7a7407caaf1d16f26aad33e4cc57d
shirin najafi <shinajafi401@gmail.com>
2022-10-21 17:02:20 +0330

    creat todo array

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,43 @@ let overlayLogin = document.querySelector(".login");
 let numberDay = document.querySelectorAll(".numberWeek");
 let days = document.querySelectorAll("[data-days]");
 let hello = document.getElementById("hello");
+let todo_Footer= document.querySelector('.todo_Footer')
+let add_Btn= document.getElementById('addButton')
+let todo_Timer= document.querySelector('.timer_Footer')
+
+let page_Name=''
+let todo_array=[
+  {day:'sat',
+   task:[],
+  },
+  {day:'sun',
+   task:[],
+  },
+  {day:'mon',
+   task:[],
+  },
+  {day:'tue',
+   task:[],
+  },
+  {day:'wed',
+   task:[],
+  },
+  {day:'thu',
+   task:[],
+  },
+  {day:'fri',
+   task:[],
+  },
+]
 
 function check() {
   if (localStorage.getItem("userInfo")) {
     overlayLogin.style.display = "none";
-    let username = JSON.parse(localStorage.getItem("userInfo")).userNamegit;
+    let username = JSON.parse(localStorage.getItem("userInfo")).userName;
     hello.innerHTML = `سلام ${username} `;
   }
 }
 addEventListener("load", check);
-check();
 
 function validationMobile(event) {
   event.preventDefault();
@@ -53,9 +80,31 @@ function getInfo() {
 }
 
 days.forEach((item) => {
-  function chooseDay() {
+  function chooseDay(event) {
     days.forEach((el) => el.classList.remove("currentDay"));
     item.classList.add("currentDay");
+
+    page_Name=event.target.dataset.days
   }
   item.addEventListener("click", chooseDay);
 });
+
+
+function add_Btn_handler(){
+
+  let newObj={
+    id: Date.now(),
+    taskName:todo_Footer.value,
+    taskTime:todo_Timer.value,
+    isDone:false
+  }
+
+ let index= todo_array.findIndex(item => item.day===page_Name)
+ console.log(index);
+
+ todo_array[index].task.push(newObj)
+ console.log( todo_array);
+
+}
+
+add_Btn.addEventListener('click', add_Btn_handler)
